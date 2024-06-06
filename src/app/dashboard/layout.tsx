8018ea87e7b99d4fb82ba0e181dc53abbf0367c5
alejandro <dban095@hotmail.com>
2024-06-05 22:48:55 -0400

    feat(theme) add dark theme

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 
 const links = [
   { name: "accordion", href: "accordion" },
+  { name: "alert dialog", href: "alertDialog" },
   { name: "alert", href: "alert" },
-  { name: "alertDialog", href: "alertDialog" },
   { name: "avatar", href: "avatar" },
   { name: "badge", href: "badge" },
   { name: "buttons", href: "buttons" },
@@ -18,10 +18,10 @@ const links = [
   { name: "combobox", href: "combobox" },
   { name: "command", href: "command" },
   { name: "context menu", href: "context-menu" },
-  { name: "data-table", href: "data-table" },
+  { name: "data table", href: "data-table" },
   { name: "dialog", href: "dialog" },
   { name: "form", href: "form" },
-  { name: "input-opt", href: "input-opt" },
+  { name: "input OTP", href: "input-otp" },
   { name: "menu bar", href: "menu-bar" },
   { name: "progress", href: "progress" },
   { name: "sheet", href: "sheet" },
@@ -29,6 +29,7 @@ const links = [
   { name: "slider", href: "slider" },
   { name: "sonner", href: "sonner" },
   { name: "tabs", href: "tabs" },
+  { name: "theme", href: "theme" },
   { name: "toast", href: "toast" },
 ];
 
@@ -39,7 +40,7 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
+      <nav className="bg-white border-b border-gray-200 fixed z-30 w-full dark:bg-slate-900 dark:border-slate-700">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
             <div className="flex items-center justify-start">
@@ -119,28 +120,31 @@ export default function DashboardLayout({
             <div className="flex items-center">
               {/* User Avatar */}
               <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
             </div>
           </div>
         </div>
       </nav>
-      <div className="flex overflow-hidden bg-white pt-16">
+      <div className="flex overflow-hidden bg-white pt-16 dark:bg-slate-900">
         <aside
           id="sidebar"
           className="fixed hidden z-20 h-full top-0 left-0 pt-16 lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
           aria-label="Sidebar"
         >
-          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white pt-0">
+          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white  pt-0 dark:bg-slate-900">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex-1 px-3 bg-white divide-y space-y-1">
+              <div className="flex-1 px-3 bg-white  divide-y space-y-1 dark:bg-slate-900">
                 <ul className="space-y-2 pb-2">
                   {links.map((link) => (
                     <li key={link.href}>
                       <Link
                         href={link.href}
-                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
+                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-slate-300"
                       >
                         <span className="ml-3">{link.name}</span>
                       </Link>
@@ -157,18 +161,18 @@ export default function DashboardLayout({
         ></div>
         <div
           id="main-content"
-          className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64"
+          className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64 dark:bg-black"
         >
           <main>
             <div className="pt-6 px-4">
               <div className="w-full min-h-[calc(100vh-230px)]">
-                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 dark:bg-slate-900">
                   {children}
                 </div>
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
+          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden dark:bg-slate-900">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
